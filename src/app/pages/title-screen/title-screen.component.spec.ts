83a2ddbdb6c383049342a8b7c39fa864ab4ec227
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2021-05-26 13:31:24 +0300

    Preparation and update to Angular 12

--- src/app/pages/title-screen/title-screen.component.spec.ts
+++ src/app/pages/title-screen/title-screen.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TitleScreenComponent } from './title-screen.component';
 
@@ -6,7 +6,7 @@ describe('TitleScreenComponent', () => {
   let component: TitleScreenComponent;
   let fixture: ComponentFixture<TitleScreenComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TitleScreenComponent ]
     })
