629b0602551d30a2690262254c581479c10991ff
kallelongjuhani <kalle.jarvenpaa@gmail.com>
2021-06-01 11:36:32 +0300

    GH Pages deploy

---
+++ deploy-to-github.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# See: https://dzone.com/articles/how-to-deploy-an-angular-application-to-github
+# ng build --configuration production --base-href https://kaljarv.github.io/the-agile-quest/
+
+# git checkout -b gh-pages
+git switch gh-pages
+ng build --output-path docs --base-href /the-agile-quest/
+cp docs/index.html docs/404.html
+git add .
+git commit -m "Deploy to GH pages"
+git push --set-upstream origin gh-pages
+git switch master
